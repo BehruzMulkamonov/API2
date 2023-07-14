5733bb4c652acca11f478eb1fe89e69f5bc495a1
BehruzMulkamonov <126500929+BehruzMulkamonov@users.noreply.github.com>
2023-07-14 20:51:54 +0500

    ignorelar bor Api

---
+++ api_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Talaba(models.Model):
+    ism = models.CharField(max_length=122)
+    familya = models.CharField(max_length=122)
+    age= models.IntegerField()
+    image = models.ImageField()
+    phone = models.CharField(max_length=13)
+    
+    def __str__(self) -> str:
+        return self.ism
